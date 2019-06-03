72f4d222e3fdfc918d29681f42b36d0fa8cbe468
silentzmb <393423939@qq.com>
2019-06-03 19:35:16 +0800

    修改配置文件

--- code/config/prod.env.js
+++ code/config/prod.env.js
@@ -7,7 +7,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
+  NODE_ENV: '"production"',
   BASE_API: '"https://api-dev.gzxiyingyun.com"',
 })
 
