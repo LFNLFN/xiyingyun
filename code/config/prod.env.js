cee9b954962d8f398a69b5b0b427507a0ee1ee78
LFN <124547864@qq.com>
2019-09-16 11:51:52 +0800

    生产环境链接更改

--- code/config/prod.env.js
+++ code/config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://api-dev.gzxiyingyun.com"',
-  OSS_BUCKET: '"estate-dev"',
+  BASE_API: '"https://api.gzxiyingyun.com"',
+  OSS_BUCKET: '"estate-prod"',
   OSS_ENDPOINT: '"oss-cn-shenzhen.aliyuncs.com"'
 }
