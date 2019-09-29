b7894a1880cb28636cd561e4bbb9f197d5790c9e
LFN <124547864@qq.com>
2019-09-29 12:21:26 +0800

    base api change

--- code/config/dev.env.js
+++ code/config/dev.env.js
@@ -4,9 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://api-dev.gzxiyingyun.com"',
-  // BASE_API: '"https://api.gzxiyingyun.com"',
+  BASE_API: '"https://project-api-dev.gzxiyingyun.com"',
   OSS_BUCKET: '"estate-dev"',
-  // OSS_BUCKET: '"estate-prod"',
   OSS_ENDPOINT: '"oss-cn-shenzhen.aliyuncs.com"'
 })
