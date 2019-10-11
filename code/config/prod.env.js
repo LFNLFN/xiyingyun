e3795b1989175a49adde1aaf3b3948f076bf3dc2
LFN <124547864@qq.com>
2019-10-11 16:07:19 +0800

    change base api

--- code/config/prod.env.js
+++ code/config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://project-api.gzxiyingyun.com"',
-  BASE_API: '"https://project-api-dev.gzxiyingyun.com"',
+  // BASE_API: '"https://api.gzxiyingyun.com"',
+  BASE_API: '"https://api-dev.gzxiyingyun.com"',
   // OSS_BUCKET: '"estate-prod"',
   OSS_BUCKET: '"estate-dev"',
   OSS_ENDPOINT: '"oss-cn-shenzhen.aliyuncs.com"'
